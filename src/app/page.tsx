833473119f5125e28274a85848004f894cf61c25
endzhe23 <endzhe.yarullina@gmail.com>
2024-05-20 00:01:13 +0300

    reformat code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+import {useEffect, useState} from "react";
+import {axiosInstance} from "@/lib/axios";
+
 type Author = {
     id: number,
     name: string;
@@ -5,16 +9,34 @@ type Author = {
 
 type Book = {
     id: string,
-    author: Author[];
+    authors: Author[];
     title: string;
     description: string;
     ISBN: string;
 };
 
 export default function Home() {
+    const [books, setBooks] = useState<Book[]>([]);
+
+    useEffect(() => {
+        axiosInstance.get('/books').then((response) => {
+            setBooks(response.data);
+        })
+            .catch((error) => {
+                console.error('Error fetching posts:', error);
+            });
+    }, [])
+
     return (
         <main className="flex min-h-screen flex-col items-left justify-between p-24">
-
+            {books.map((book) => (
+                <ul key={book.id}>
+                    <li>Название книги:{book.title}</li>
+                    <li>Описание книги: {book.description}</li>
+                    <li>ISBN книги: {book.ISBN}</li>
+                    <li>Авторы книги: {book.authors.map((author: { name: any; }) => (author.name))}</li>
+                </ul>
+            ))}
         </main>
     );
 }
